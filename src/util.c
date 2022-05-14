fabca5f49515f10a2f594a2430e8a236ab723bb7
Chris Hixon <ChrisHixon@users.noreply.github.com>
2022-05-14 07:47:50 -0600

    Add read_file function

--- src/util.c
+++ src/util.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "mem.h"
 #include "util.h"
@@ -69,4 +70,107 @@ char *esc_bytes(const unsigned char *bytes, int length, int limit)
     if (trunc) { *p++ = '.'; *p++ = '.'; *p++ = '.'; }
     *p = '\0';
     return res;
+}
+
+#ifndef CHPEG_READ_FILE_BLOCKSIZE
+#define CHPEG_READ_FILE_BLOCKSIZE 4096
+#endif
+
+#ifndef CHPEG_READ_FILE_INITIAL_SIZE
+#define CHPEG_READ_FILE_INITIAL_SIZE 4096 * 4
+#endif
+
+int read_file(const char *filename, unsigned char **data, size_t *length)
+{
+    int ret = 0;
+    size_t bsize = 0, remain = 0, bytes_read = 0, len = 0;
+    unsigned char *buf = NULL, *p = NULL;
+    FILE *fp;
+
+    // return error if either data or length return address is NULL
+    if (data == NULL || length == NULL) {
+        return 1;
+    }
+
+    // read from stdin if filename is NULL or "-"
+    if (filename == NULL || strcmp(filename, "-") == 0) {
+        filename = NULL;
+        fp = stdin;
+    }
+    // else, open file
+    else {
+        fp = fopen(filename, "r");
+        if (fp == NULL) {
+            perror(filename);
+            ret = 1;
+            goto cleanup;
+        }
+    }
+
+    // initial allocation
+    buf = malloc(CHPEG_READ_FILE_INITIAL_SIZE);
+    if (buf == NULL) {
+        perror("malloc");
+        ret = 1;
+        goto cleanup;
+    }
+    p = buf;
+    bsize = remain = CHPEG_READ_FILE_INITIAL_SIZE;
+
+    for (;;) {
+        bytes_read = fread(p, 1, remain < CHPEG_READ_FILE_BLOCKSIZE ?
+            remain : CHPEG_READ_FILE_BLOCKSIZE, fp);
+        if (bytes_read > 0) {
+            remain -= bytes_read;
+            p += bytes_read;
+            len += bytes_read;
+            if (remain == 0) {
+                buf = realloc(buf, bsize * 2);
+                if (buf == NULL) {
+                    perror("realloc");
+                    ret = 1;
+                    break;
+                }
+                p = buf + bsize;
+                remain = bsize;
+                bsize *= 2;
+            }
+        }
+        else {
+            if (feof(fp)) {
+                ret = 0;
+            }
+            else if (ferror(fp)) {
+                fprintf(stderr, "Error reading file: %s\n",
+                    filename ? filename : "<stdin>");
+                ret = 1;
+            }
+            else {
+                fprintf(stderr, "Unknown error reading file: %s\n",
+                    filename ? filename : "<stdin>");
+                ret = 1;
+            }
+            break;
+        }
+    }
+
+cleanup:
+    if (filename && fp) { // close only if fopen'd a file
+        fclose(fp);
+    }
+    if (ret) { // if we're returning error
+        if (buf) {
+            free(buf); // free the allocated buffer
+        }
+        *data = NULL;
+        *length = 0;
+    }
+    else {
+        if (len > CHPEG_READ_FILE_INITIAL_SIZE) {
+            buf = realloc(buf, len);
+        }
+        *data = buf;
+        *length = len;
+    }
+    return ret;
 }
--- src/util.h
+++ src/util.h
@@ -2,5 +2,6 @@
 #define CHPEG_UTIL_H
 
 extern char *esc_bytes(const unsigned char *bytes, int length, int limit);
+extern int read_file(const char *filename, unsigned char **data, size_t *length);
 
 #endif // #ifndef CHPEG_UTIL_H
