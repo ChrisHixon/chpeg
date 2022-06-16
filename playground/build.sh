64aa640072dd1cd55c400df1a350222428fc2a93
Chris Hixon <ChrisHixon@users.noreply.github.com>
2022-06-16 11:10:04 -0600

    -sALLOW_MEMORY_GROWTH

--- playground/build.sh
+++ playground/build.sh
@@ -5,6 +5,7 @@ umask 022
 emcc -DCHPEG_PACKRAT -DCHPEG_EXTENSION_ALL -DCHPEG_VM_PROFILE \
     -std=c99 -O3 -Wall -Wno-unused-function -pedantic \
     -sEXPORTED_FUNCTIONS=_parse -sEXPORTED_RUNTIME_METHODS=ccall,cwrap \
+    -sALLOW_MEMORY_GROWTH \
     chpeg-playground.c -o chpeg-playground.js
 
 # --pre-js chpeg-pre.js \
